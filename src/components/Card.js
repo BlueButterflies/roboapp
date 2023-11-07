b29cce32aef52d318cb3619511c1e6cf11fd1b55
Butterfly Tech Creative Solution <52591976+BlueButterflies@users.noreply.github.com>
2023-11-07 09:35:07 +0100

    update react version

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-
 const Card = ({id, name, email}) => {
     return (
-        <div className="bg-light-pink dib br4 ma3 grow shadow- o-60">
+        <div className="dib br4 ma3 grow shadow- o-60">
             <img src={`https://robohash.org/${id}?80x80`} alt="robots" />
             <div className="i-m b tc white-80" >
                 <h2>{name}</h2>
